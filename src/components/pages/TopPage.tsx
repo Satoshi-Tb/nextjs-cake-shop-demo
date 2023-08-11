59d5bc4f0fdd6242a1a37b55addf6df692e6fbb0
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2023-08-12 00:06:41 +0900

    タブパネル実装

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,11 +1,59 @@
 import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, ThemeProvider } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  ThemeProvider,
+  Tabs,
+  Tab,
+  Box,
+} from "@mui/material";
 import { cakeTableSetting, initialCakeData } from "@/data/cakeData";
 import { ListTable } from "../model/ListTable";
 import commonStyle from "@/styles/commonStyle";
+import { initialMaterialData, materialTableSetting } from "@/data/materialData";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 export const TopPage = () => {
+  const [value, setValue] = useState(0);
   const [funds, setFunds] = useState(10000);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <ThemeProvider theme={commonStyle}>
       <AppBar position="static" color="primary">
@@ -18,12 +66,30 @@ export const TopPage = () => {
       <Typography variant="h3" color="secondary">
         現在の資金:{funds}円
       </Typography>
-      <AppBar position="static" color="default">
-        <Typography variant="h6" color="inherit">
-          販売ケーキ一覧|在庫管理
-        </Typography>
-      </AppBar>
-      <ListTable tableSetting={cakeTableSetting} itemData={initialCakeData} />
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="販売ケーキ一覧" {...a11yProps(0)} />
+            <Tab label="在庫管理" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <ListTable
+            tableSetting={cakeTableSetting}
+            itemData={initialCakeData}
+          />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <ListTable
+            tableSetting={materialTableSetting}
+            itemData={initialMaterialData}
+          />
+        </CustomTabPanel>
+      </Box>
     </ThemeProvider>
   );
 };
--- src/styles/commonStyle.tsx
+++ src/styles/commonStyle.tsx
@@ -1,5 +1,4 @@
 import { createTheme } from "@mui/material/styles";
-import amber from "@mui/material/colors/amber";
 import { orange, purple } from "@mui/material/colors";
 
 const commonStyle = createTheme({
